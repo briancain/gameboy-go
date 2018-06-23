4e1cf0d03ee1cdee7e4551b18f89917b77025217
Brian Cain <brianccain@gmail.com>
2018-06-23 10:33:50 -0700

    Add function to display current CPU frame

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,6 +1,9 @@
 package cpu
 
-import ()
+import (
+	"fmt"
+	"log"
+)
 
 type Registers struct {
 	// 8-bit registers
@@ -11,11 +14,9 @@ type Registers struct {
 	E byte
 	F byte // Flag register
 	// 16-bit registers
-	PC byte // Program Counter
-	SP byte // Stack Pointer
-	// Clock for last instruction
-	M byte
-	T byte
+	PC                   byte // Program Counter
+	SP                   byte // Stack Pointer
+	LastInstructionClock Clock
 }
 
 type Clock struct {
@@ -25,10 +26,35 @@ type Clock struct {
 
 // Game Boy cpu type Z80
 type Z80 struct {
-  regiesters Registers
-  clock Clock
+	reg   Registers
+	clock Clock
 }
 
-func Display() string {
-	return "Hello"
+func ResetClock(clock *Clock) {
+	log.Print("[DEBUG] Resetting clock")
+	clock.m = 0
+	clock.t = 0
+}
+
+func ResetCPU(cpu *Z80) {
+	cpu.reg.A = 0
+	cpu.reg.B = 0
+	cpu.reg.C = 0
+	cpu.reg.D = 0
+	cpu.reg.E = 0
+	cpu.reg.F = 0
+	cpu.reg.PC = 0
+	cpu.reg.SP = 0
+	ResetClock(&cpu.reg.LastInstructionClock)
+}
+
+func DisplayCPUFrame(cpu Z80) string {
+	return fmt.Sprintf("A: %X\nB: %X\nC: %X\nD: %X\nE: %X\nF: %X",
+		cpu.reg.A, cpu.reg.B, cpu.reg.C, cpu.reg.D, cpu.reg.E, cpu.reg.F)
+}
+
+func NewCPU() *Z80 {
+	cpu := new(Z80)
+	ResetCPU(cpu)
+	return cpu
 }
--- main.go
+++ main.go
@@ -4,22 +4,14 @@ import (
 	"github.com/briancain/gameboy-go/cpu"
 	"github.com/briancain/gameboy-go/version"
 	"log"
-	"os"
-
-	"github.com/hashicorp/logutils"
 )
 
 func main() {
-	filter := &logutils.LevelFilter{
-		Levels:   []logutils.LogLevel{"DEBUG", "WARN", "ERROR"},
-		MinLevel: logutils.LogLevel("WARN"),
-		Writer:   os.Stderr,
-	}
-	log.SetOutput(filter)
 
 	log.Print("Starting gameboy-go ... ")
 	version := version.Get()
 	log.Print("Version loaded: ", version)
-	display := cpu.Display()
-	log.Print(display)
+	gbcpu := cpu.NewCPU()
+	display := cpu.DisplayCPUFrame(*gbcpu)
+	log.Print("CPU Frame:\n", display)
 }
